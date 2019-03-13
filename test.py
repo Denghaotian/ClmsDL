d54974dc1d0fce714f16d8c7ce36a044cf399fbc
qingbol <qingbol@g.clemson.edu>
2019-03-12 22:30:57 -0400

    restore the saved model

--- test.py
+++ test.py
@@ -32,7 +32,23 @@ def main(_):
     # We shall load all the training and validation images and labels into memory using openCV and use that during training
     x_batch, y_true_batch, _, cls_batch, categories = load_dataset(FLAGS.test_path, FLAGS.img_size, classes)
 
+    with tf.Session() as sess
+        ## Let us restore the saved model 
+        # Step-1: Recreate the network graph. At this step only graph is created.
+        saver = tf.train.import_meta_graph(FLAGS.trained_model)
+        # Step-2: Now let's load the weights saved using the restore method.
+        saver.restore(sess, tf.train.latest_checkpoint(FLAGS.checkpoint))
 
+        # Accessing the default graph which we have restored
+        graph = tf.get_default_graph()
+
+        # Now, let's get hold of the op that we can be processed to get the output.
+        # In the original network labels_pred is the tensor that is the prediction of the network
+        labels_pred = graph.get_tensor_by_name("labels_pred:0")
+
+        ## Let's feed the images to the input placeholders
+        data_placeholder= graph.get_tensor_by_name("data_placeholder:0") 
+        label_placeholder = graph.get_tensor_by_name("label_placeholder:0") 
 
 
 if __name__ == "__main__":
@@ -42,5 +58,7 @@ if __name__ == "__main__":
     flags.DEFINE_string("test_path", "data_cat/testing_data", "path of testing data")
     flags.DEFINE_integer('img_size', 128, 'image width=image height.')
     flags.DEFINE_string("output_labels", "trained_model/output_labels.txt", "store the labels")
+    flags.DEFINE_string("trained_model", "trained_model/dogs-cats-model.meta", "meta graph")
+    flags.DEFINE_string("checkpoint", "./trained_model/", "checkpoint")
 
     tf.app.run()
